a54c5ef95abd4882dcd481cead6367e5712edd79
nicholastwai <57726227+nicholastwai@users.noreply.github.com>
2023-04-11 16:04:54 -0400

    added automatic versioning

--- feature_pipeline.py
+++ feature_pipeline.py
@@ -1,3 +1,6 @@
+'''
+convert this into file for local use as feature pipeline so its compatible with github actions
+'''
 # -*- coding: utf-8 -*-
 """feature_pipeline.ipynb
 
@@ -9,7 +12,7 @@ Original file is located at
 #Installing Data Scraper
 """
 
-!pip install -e git+https://github.com/nicholastwai/UFC_Data_Scraper.git#egg=UFC_Data_Scraper 
+#!pip install -e git+https://github.com/nicholastwai/UFC_Data_Scraper.git#egg=UFC_Data_Scraper 
 
 # note to self: must change location of starting_dir = os.getcwd() to line 49 of scraper.py so that starting_dir is initalized before,
 # must also reconnect runtime so that changes are registered
@@ -23,7 +26,7 @@ Original file is located at
 
 # Commented out IPython magic to ensure Python compatibility.
 # %cd /content/drive/MyDrive/Capstone\ Project/End-to-End\ ML \Service
-!ls
+#!ls
 
 import pandas as pd  
 from UFC_Data_Scraper import scraper as s #Creating instance of Ufc_Data_Scraper object
@@ -36,7 +39,6 @@ scraper = s.Ufc_Data_Scraper()
 list_of_fighters = scraper.get_all_fighters()
 json_fighter_list = s.Ufc_Data_Scraper().fighter_to_Json(list_of_fighters)
 athletes_df = pd.DataFrame.from_dict(json_fighter_list).transpose()
-athletes_df
 
 """# Scraping event data and moving it to dataframe"""
 
@@ -44,7 +46,6 @@ athletes_df
 list_of_events =  s.Ufc_Data_Scraper().scrape_all_fights(load_from_dir = False)
 event_dict = s.Ufc_Data_Scraper().fights_to_Json(list_of_events)
 all_events_df = pd.DataFrame.from_dict(event_dict).transpose()
-all_events_df
 
 """# Data Cleaning
 - taking appropriate code from fight_proj_EDA to create dataset
@@ -60,7 +61,7 @@ athletes_df.drop(indexNullStance, inplace = True)
 
 indexNullHeight= athletes_df[athletes_df['height']== '--'].index # null height
 athletes_df.drop(indexNullHeight, inplace = True)
-athletes_df.shape
+
 
 # dropping td defense, birthdate, and no_contest columns
 athletes_df.drop(columns = ['no_contest', 'DOB', 'takedown_defense', 'name'], inplace = True)
@@ -81,7 +82,6 @@ athletes_df['height'] = height_inches
 # typecasting everything from string to int
 athletes_df = athletes_df.apply(pd.to_numeric)
 
-athletes_df
 
 """# Creating fighter matchups for final fight dataset to train models on"""
 
@@ -105,7 +105,7 @@ for i in range(all_events_df.shape[0]):
       except:
         pass
 df = df.transpose()
-df
+
 
 # setting index so that dropping invalid rows by indices works
 df.reset_index(inplace=True)
@@ -114,7 +114,7 @@ df.drop(inplace = True, columns = ['index'])
 #dropping rows where the winner column resulted in an error
 for i in range(0, len(df['winner'])):
   if df['winner'][i] == -1:
-    df = df.drop([i]
+    df = df.drop([i])
 
 # setting index again after dropping the ones with invalid winner
 df.reset_index(inplace=True)
@@ -122,21 +122,30 @@ df.drop(inplace = True, columns = ['index'])
 
 # creating index column for Hopsworks primary key
 df.reset_index(inplace=True)
-df
+
 
 """# Moving data set to feature store (incorporating Hopsworks)"""
 
-!pip install -U hopsworks --quiet
-
 import hopsworks
 
-project = hopsworks.login(api_key_file = '/content/drive/MyDrive/Capstone Project/End-to-End ML Service/.hw_api_key')
+project = hopsworks.login(api_key_file = '.hw_api_key')
 
 fs = project.get_feature_store()
 
+#incrementing data version number
+import os
+if not os.path.exists('version.txt'):
+    with open('version.txt','w') as f:
+        f.write('0')
+with open('version.txt','r') as f:
+    version_num = int(f.read())
+    version_num +=1 
+with open('version.txt','w') as f:
+    f.write(str(version_num))
+
 fight_data_fg = fs.get_or_create_feature_group(
     name="fight_data",
-    version=1,
+    version=version_num,
     description="Fight data for bout outcome prediction.",
     primary_key = ['index'],
 )
@@ -164,5 +173,3 @@ feature_descriptions = [
 
 for desc in feature_descriptions: 
     fight_data_fg.update_feature_description(desc["name"], desc["description"])
-
-print("forkin"+"me")
